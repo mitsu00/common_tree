1d9196439744660a706e77fc01f6c55ca5397bf3
bgcngm <bgcngm@users.noreply.github.com>
2023-11-06 19:26:46 +0700

    mt6768-common: Migrate to AIDL ClearKey DRM HAL
    Signed-off-by: 01soni247 <01soni247@gmail.com>

--- mt6768.mk
+++ mt6768.mk
@@ -94,7 +94,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 PRODUCT_PACKAGES += \
     libmockdrmcryptoplugin
